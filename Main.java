5da9378330d30e266390e2b75a664f80fac52e81
YaphetYonas1 <85519133+YaphetYonas5@users.noreply.github.com>
2021-12-28 07:20:06 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("A method in Java is a block of statements that has a name and can be executed by calling it from some other place in your program. Methods are used to divide complicated programs into manageable pieces.");
+
+    System.out.println("Void methods - methods that do not have a return data type. These methods do not use a return statement to return a value. Value-returning methods - methods that have a return data type. These methods return a value of a specific data type using the return statement.");
+  }
+}
